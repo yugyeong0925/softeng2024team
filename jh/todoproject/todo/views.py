e25b5478c505bb300248d33b7e4473c6b645f934
kltpls <93664985+kltpls@users.noreply.github.com>
2024-11-09 19:58:05 +0900

    Add files via upload

---
+++ jh/todoproject/todo/views.py
@@ -0,0 +1,38 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.shortcuts import render, redirect
+from .models import Task
+from .forms import TaskForm
+
+def task_list(request):
+    tasks = Task.objects.all()
+    return render(request, 'todo/task_list.html', {'tasks': tasks})
+
+def task_create(request):
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('task_list')
+    else:
+        form = TaskForm()
+    return render(request, 'todo/task_form.html', {'form': form})
+
+def task_update(request, pk):
+    task = Task.objects.get(id=pk)
+    if request.method == 'POST':
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+            return redirect('task_list')
+    else:
+        form = TaskForm(instance=task)
+    return render(request, 'todo/task_form.html', {'form': form})
+
+def task_delete(request, pk):
+    task = Task.objects.get(id=pk)
+    if request.method == 'POST':
+        task.delete()
+        return redirect('task_list')
+    return render(request, 'todo/task_confirm_delete.html', {'task': task})
